736677a1736d79e9558eff40bb080e8070bab8f6
ali <basala528@gmail.com>
2025-10-21 11:34:56 +0300

    animation work

---
+++ My project/Assets/PlayerController.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public KeyCode Spacebar;
+    public KeyCode L;
+    public KeyCode R;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private bool grounded;
+    private Animator anim;
+    private Rigidbody2D rb;
+    private SpriteRenderer sprite;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Spacebar) && grounded)
+            Jump();
+
+        if (Input.GetKey(L))
+        {
+            rb.velocity = new Vector2(-moveSpeed, rb.velocity.y);
+            sprite.flipX = true;
+        }
+
+        if (Input.GetKey(R))
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+            sprite.flipX = false;
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+        anim.SetFloat("Height", rb.velocity.y);
+        anim.SetBool("Grounded", grounded);
+    }
+
+    void FixedUpdate()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+    }
+}
